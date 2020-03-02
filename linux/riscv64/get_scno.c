1af57c3eb5be65a03397141e5d4ae532a8ea2a25
Alistair Francis <alistair.francis@wdc.com>
2020-03-02 14:33:51 -0800

    linux/riscv: Split out generic RISC-V code
    Split out the generic RISC-V code in preperation for adding RV32
    support.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- linux/riscv64/get_scno.c
+++ linux/riscv64/get_scno.c
@@ -5,10 +5,4 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
-/* Return codes: 1 - ok, 0 - ignore, other - error. */
-static int
-arch_get_scno(struct tcb *tcp)
-{
-	tcp->scno = riscv_regs.a7;
-	return 1;
-}
+#include "riscv/get_scno.c"
--- linux/riscv64/set_scno.c
+++ linux/riscv64/set_scno.c
@@ -5,11 +5,4 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
-static int
-arch_set_scno(struct tcb *tcp, kernel_ulong_t scno)
-{
-	if (ptrace_syscall_info_is_valid() && get_regs(tcp) < 0)
-		return -1;
-	riscv_regs.a7 = scno;
-	return set_regs(tcp->pid);
-}
+#include "riscv/set_scno.c"
